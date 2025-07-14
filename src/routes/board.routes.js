928b8842d3f211f60cf4b8e342f26d7aef9468d5
luongcongduy97 <duylcong97@gmail.com>
2025-07-14 18:58:30 +0700

    update board route

--- src/routes/board.routes.js
+++ src/routes/board.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const boardController = require('../controllers/board.controller');
 const authMiddleware = require('../middleware/auth.middleware');
+const adminMiddleware = require('../middleware/admin.middleware');
 
-router.post('/:teamId/boards', authMiddleware, boardController.createBoard);
+router.post('/:teamId/boards', authMiddleware, adminMiddleware, boardController.createBoard);
 router.get('/:teamId/boards', authMiddleware, boardController.getBoardsByTeam);
 
 module.exports = router;
