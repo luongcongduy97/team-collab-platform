0557661c5fd90107fab14094a796ae8f78aa864d
luongcongduy97 <duylcong97@gmail.com>
2025-07-24 20:50:33 +0700

    update chat test

--- cypress/e2e/chat.cy.js
+++ cypress/e2e/chat.cy.js
@@ -28,6 +28,17 @@ describe('Team Chat (UI)', () => {
       },
     });
 
+    // join-team should be emitted when the page loads
+    cy.get('@emit').should('have.been.calledWithMatch', 'join-team', {
+      teamId: '1',
+    });
+
+    // wait for the page to register socket listeners
+    cy.wrap(null).should(() => {
+      expect(socketStub.events['messages']).to.be.a('function');
+    });
+
+    // emit existing messages
     cy.then(() => {
       socketStub.events['messages']([{ id: 1, content: 'Hello there', user: { name: 'Tester' } }]);
     });
@@ -40,5 +51,20 @@ describe('Team Chat (UI)', () => {
     cy.get('@emit').should('have.been.calledWithMatch', 'send-message', {
       content: 'Howdy',
     });
+
+    // simulate server broadcasting the new message
+    cy.wrap(null).should(() => {
+      expect(socketStub.events['new-message']).to.be.a('function');
+    });
+
+    cy.then(() => {
+      socketStub.events['new-message']({
+        id: 2,
+        content: 'Howdy',
+        user: { name: 'Chat User' },
+      });
+    });
+
+    cy.contains('Chat User:').parent().contains('Howdy').should('exist');
   });
 });
